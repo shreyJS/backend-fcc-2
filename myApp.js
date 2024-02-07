d93295c658f65d917588e4567927f53fa484c1a0
shreyJS <deshpandeshreyas850@gmail.com>
2024-02-08 01:20:20 +0530

    setting up express server

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
+app.get("/", (req, res) => res.send("Hello Express"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
